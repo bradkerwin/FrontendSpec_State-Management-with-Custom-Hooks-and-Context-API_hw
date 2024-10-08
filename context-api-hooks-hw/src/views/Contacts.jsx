d146198287395371ad94772cbf604b9240af998c
bradkerwin <bkerwin101@gmail.com>
2024-10-07 22:06:48 -0400

    modified contact display

--- context-api-hooks-hw/src/views/Contacts.jsx
+++ context-api-hooks-hw/src/views/Contacts.jsx
@@ -5,12 +5,16 @@ import { useContactContext } from '../context/ContactContext'
 const Contacts = () => {
     const { contacts } = useContactContext()
 
+    const handleDisplayNumber = () => {
+      {contacts.phoneNumber}
+    }
+
   return (
     <Container>
       <h1>Your Contacts: </h1>
       {contacts.map(contact => (
         <div>
-          <li>{contact.name}</li>
+          <li className='name' onClick={handleDisplayNumber()}>{contact.name}</li>
           <li>{contact.phoneNumber}</li>
         </div>
       ))
